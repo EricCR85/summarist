b3e9d617d549e45e429065d2fc5d6819cb3f4e9e
EricCR85 <ecr.ceo@gmail.com>
2026-06-09 04:23:37 -0400

    update for you page

---
+++ app/for-you/page.jsx
@@ -0,0 +1,8 @@
+export default function ForYouPage() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>For You Page</h1>
+      <p>Welcome to your personalized feed!</p>
+    </div>
+  );
+}
