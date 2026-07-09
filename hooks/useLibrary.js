8e294e2659408c1be93ab3a7cfb156a439146848
EricCR85 <ecr.ceo@gmail.com>
2026-07-08 23:31:08 -0400

    add to library

--- hooks/useLibrary.js
+++ hooks/useLibrary.js
@@ -4,51 +4,60 @@ import { db, auth } from "../firebase";
 import { addDoc, deleteDoc, doc } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
 
-
 export const useLibrary = () => {
   const [library, setLibrary] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-  const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
-    if (!user) {
-      setLibrary([]);
-      setLoading(false);
-      return;
-    }
+    const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        setLibrary([]);
+        setLoading(false);
+        return;
+      }
 
-    const q = query(
-      collection(db, "library"),
-      where("userId", "==", user.uid)
-    );
+      const q = query(
+        collection(db, "library"),
+        where("userId", "==", user.uid),
+      );
 
-    const unsubscribeLibrary = onSnapshot(q, (snapshot) => {
-      const booksData = snapshot.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
+      const unsubscribeLibrary = onSnapshot(q, (snapshot) => {
+        const booksData = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
 
-      setLibrary(booksData);
-      setLoading(false);
+        setLibrary(booksData);
+        setLoading(false);
+      });
+
+      return unsubscribeLibrary;
     });
 
-    return unsubscribeLibrary;
-  });
-
-  return () => unsubscribeAuth();
-}, []);
-
-
+    return () => unsubscribeAuth();
+  }, []);
 
   const addToLibrary = async (book) => {
-    if (!auth.currentUser) {
+    const user = auth.currentUser;
+
+    if (!user) {
       alert("please log in to add books.");
       return;
     }
-    await addDoc(collection(db, "library"), {
-      ...book,
-      userId: auth.currentUser.uid,
-    });
+
+    try {
+      console.log("Adding book to library:", book);
+      await addDoc(collection(db, "library"), {
+        ...book,
+        bookId: book.bookId || book.id,
+        userId: user.uid,
+        createdAt: new Date(),
+      });
+
+      console.log("Book added successfully");
+    } catch (error) {
+      console.error("Error adding book to library:", error);
+    }
   };
 
   const removeFromLibrary = async (docId) => {
